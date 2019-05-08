408db74d1868b8e74058b9e412a2b51606c2828f
Cubewool <ginny131@hotmail.com>
2019-05-07 22:42:56 -0700

    Push main to Github

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  C++ Project
+//
+//  Created by Ginny on 5/6/19.
+//  Copyright © 2019 Ginny. All rights reserved.
+//
+
+
+#include <iostream>
+#include "NextPermutation.h"
+
+int main(int argc, const char * argv[]) {
+    NextPermutation mysol;
+    cout << mysol.NextGreaterNum(127431)<<endl;
+    cout << mysol.NextSmallerNum(1234)<<endl;
+    return 0;
+}
